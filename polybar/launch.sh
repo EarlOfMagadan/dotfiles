a76bfcf98764ebb2241150cbc8886314e772e5e5
EarlOfMagadan <earlofmagadan@protonmail.com>
2021-08-23 13:00:14 +1200

    a lot of new shit

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,12 +2,14 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
-xrandr --output HDMI-0 --primary
-xrandr --output DVI-D-0 --right-of HDMI-0
-feh --bg-fill ~/Pictures/Wallpaper/Wallpaper.png 
+
+
+
+xrandr --output HDMI-0 --primary --left-of DVI-D-0
+feh --bg-fill ~/Pictures/Wallpaper/Wallpaper.jpg ~/Pictures/Wallpaper/WallpaperII.png
+#feh --bg-fill 
+
 
 polybar -r bar1 &
 polybar -r bar2 &
-#polybar -r bar3 &
-#polybar -r bar4 &
 done
--- polybar/scripts/updates-pacman-aurhelper.sh
+++ polybar/scripts/updates-pacman-aurhelper.sh
@@ -16,7 +16,7 @@ fi
 updates=$((updates_arch + updates_aur))
 
 if [ "$updates" -gt 0 ]; then
-    echo " $updates"
+    echo "$updates"
 else
     echo ""
 fi
