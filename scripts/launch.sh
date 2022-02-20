07cbe9d3a11d2cf2fb9d6d937c570376d6eb8cc9
EarlOfMagadan <earlofmagadan@protonmail.com>
2022-02-21 09:51:23 +1300

    a

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,9 +4,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 xrandr --output HDMI-0 --primary --left-of DP-2
-feh --bg-fill ~/Pictures/Wallpapers/ActiveWallpapers/safe.png ~/Pictures/Wallpapers/ActiveWallpapers/safe.png
+feh --bg-fill ~/Pictures/Wallpapers/ActiveWallpapers/safeiii.png ~/Pictures/Wallpapers/ActiveWallpapers/safeii.png
 
 polybar -r -c /home/bowden/.config/polybar/config.ini tabs1 & sleep 2
-polybar -r -c /home/bowden/.config/polybar/config.ini tabs1B & sleep 2
+#polybar -r -c /home/bowden/.config/polybar/config.ini tabs1B & sleep 2
 polybar -r -c /home/bowden/.config/polybar/config.ini tabs2 & 
 done
